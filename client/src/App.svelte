9c2d8f67a350428f8b1aeb81f873ac63b5439e13
Usaid <usaid.k.tirmizi@gmail.com>
2024-12-15 02:41:39 +0500

    Setup frontend page

---
+++ client/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { onMount } from 'svelte';
+  import axiosInstance from './lib/api';
+
+  let apiName = "";
+  let isLoading = true;
+
+  onMount(async () => {
+    try {
+      const response = await axiosInstance.get('/test');
+      apiName = response.data;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      isLoading = false;
+    }
+  });
+</script>
+
+<main>
+  {#if isLoading}
+    <p>Loading...</p>
+  {:else}
+    <h1>{apiName}</h1>
+  {/if}
+</main>
